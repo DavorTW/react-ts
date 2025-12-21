804fb774964acd6fef18e6de0aabc80db0f312cb
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2025-12-21 18:55:34 +0800

    Finished the basic gif app with search functionality working and showing previous terms. Next step: implement previous search terms functionality

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,20 +1,32 @@
-import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { GifList } from "./gifs/components/GifList";
 import { useState } from "react";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.actions";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
 
-  const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const handleTermClicked = (term:string) => {
     console.log({term});
   }
 
-  const handleSearch = (query: string) => {
-    console.log({query});
+  const handleSearch = async (query: string = '') => {
+    query = query.trim().toLowerCase();
+    if(query.length === 0) return;
+    
+    if(previousTerms.includes(query)) return;
+
+    setPreviousTerms([query, ...previousTerms].splice(0,8));
+    
+    const gifs = await getGifsByQuery(query);
+
+    setGifs(gifs);
   }
 
   return (
@@ -29,7 +41,7 @@ export const GifsApp = () => {
         <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked}/>
 
         {/* Gifs */}
-        <GifList gifs={mockGifs}/>
+        <GifList gifs={gifs}/>
     </>
   );
 }
